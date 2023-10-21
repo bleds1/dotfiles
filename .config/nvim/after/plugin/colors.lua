1f077e78718ebef4dda8722b646bb9d618fb0c11
bleds1 <bleds1@users.noreply.github.com>
2023-10-21 14:42:40 +0100

    added pink colour visual mode highlight

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -5,6 +5,7 @@ function ColorMyPencils(color)
 			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"})
 			vim.api.nvim_set_hl(0, "StatusLine", { bg = "none"})
 			vim.api.nvim_set_hl(0, "SignColumn", { bg = "none"})
+            vim.api.nvim_set_hl(0, "Visual",{ bg = "#C290A0", fg = "#232323"}) 
 end
 
 ColorMyPencils()
