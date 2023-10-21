593f410e96eb9e989953cac16011e16c384c91e7
bleds1 <bleds1@users.noreply.github.com>
2023-10-21 11:16:12 +0100

    updated README.md

--- .config/nvim/lua/bledley/set.lua
+++ .config/nvim/lua/bledley/set.lua
@@ -34,7 +34,7 @@ vim.opt.breakindent = true
 
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 15
-vim.g.netrw_browse_split = 0
+vim.g.netrw_browse_split = 4
 vim.g.netrw_keepdir = 0
 vim.g.netrw_localcopydircmd = 'cp -r'
 vim.g.netrw_browsex_viewer = 'xdg-open'
--- README.md
+++ README.md
@@ -15,6 +15,9 @@
 + probably more..
 
 2023-10-21-1058
+
 *TODO* Flesh out this readme and add screenshots/links/system info/descriptions
+
 *TODO* Symlink and push latest Polybar
+
 *TODO* Symlink and push latest Vimium
