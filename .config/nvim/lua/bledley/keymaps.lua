2a0cf9e31230ad34f235c7d72a31196542bf4662
bleds1 <bleds1@users.noreply.github.com>
2023-10-22 15:37:48 +0100

    added fzf and keymaps

--- .config/nvim/lua/bledley/keymaps.lua
+++ .config/nvim/lua/bledley/keymaps.lua
@@ -3,6 +3,10 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>o-", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>ol", vim.cmd.Hex)
 vim.keymap.set("n", "<leader>oo", vim.cmd.Lex)
+vim.keymap.set("n", "<leader>.", vim.cmd.Files)
+vim.keymap.set("n", "<leader>'", vim.cmd.Buffers)
+vim.keymap.set("n", "<leader>/", vim.cmd.BLines)
+vim.keymap.set("n", "<leader>rg", vim.cmd.Rg)
 -- Buffers
 vim.keymap.set("n", "<leader>bn", vim.cmd.bn)
 vim.keymap.set("n", "<leader>bp", vim.cmd.bp)
--- .config/nvim/lua/bledley/packer.lua
+++ .config/nvim/lua/bledley/packer.lua
@@ -22,6 +22,8 @@ return require('packer').startup(function(use)
 	})
 
 	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+    use 'junegunn/fzf'
+    use 'junegunn/fzf.vim'
 	use('nvim-treesitter/playground')
 	use('theprimeagen/harpoon')
 	use('mbbill/undotree')
