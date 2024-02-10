f997b5358d6f3b96765c2bfc3407de7571164e6d
bleds1 <bleds1@users.noreply.github.com>
2024-02-10 22:06:28 +0000

    minor cleanup

--- .config/nvim/lua/bledley/set.lua
+++ .config/nvim/lua/bledley/set.lua
@@ -1,50 +1,46 @@
 -- Transparent linenumbers column
 vim.cmd('hi! LineNr guibg=none ctermbg=none')
--- disable netrw at the very start of your init.lua
+-- Disable netrw at the very start of your init.lua
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
--- nvim tree
-vim.g.nvim_tree_indent_markers = 1
 --vim.g.netrw_banner = 0
 --vim.g.netrw_winsize = 15
 --vim.g.netrw_browse_split = 0
 --vim.g.netrw_keepdir = 0
 --vim.g.netrw_localcopydircmd = 'cp -r'
+-- Nvim tree
+vim.g.nvim_tree_indent_markers = 1
 vim.g.netrw_browsex_viewer = 'xdg-open'
 vim.o.laststatus = 0
---vim.opt.nu = true
-vim.opt.number = true
-vim.opt.relativenumber = false
-vim.opt.cursorline = true
---vim.opt.guicursor = ""
---vim.opt.showtabline =2
-vim.opt.showmode = true
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-vim.opt.autoindent = true
-vim.opt.smartindent = true
-vim.opt.wrap = true
+vim.o.nu = true
+vim.o.relativenumber = false
+vim.o.cursorline = true
+--vim.o.guicursor = ""
+--vim.o.showtabline =2
+vim.o.showmode = true
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.autoindent = true
+vim.o.smartindent = true
+vim.o.wrap = true
+--if i switch below to vim.o source errors?
 vim.opt.fillchars = { eob = ' ' }
-
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-
-vim.opt.termguicolors = true
-vim.opt.scrolloff = 8
---vim.opt.signcolumn = "yes"
-
-vim.opt.updatetime = 100
-vim.opt.clipboard = "unnamedplus"
-vim.opt.mouse = 'a'
-vim.opt.breakindent = true
+vim.o.swapfile = false
+vim.o.backup = false
+vim.o.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.o.hlsearch = false
+vim.o.incsearch = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.termguicolors = true
+vim.o.scrolloff = 8
+--vim.o.signcolumn = "yes"
+vim.o.updatetime = 100
+vim.o.clipboard = "unnamedplus"
+vim.o.mouse = 'a'
+vim.o.breakindent = true
 -- vimwiki 
 vim.g.vimwiki_list = {{path = '~/Dropbox/vault/', syntax = 'markdown', ext = '.md'}}
 --vim.g.vimwiki_markdown_link_ext = true
