fc2e2185951fbb0d539877a9b8cefe61e6257d86
bleds1 <bleds1@users.noreply.github.com>
2024-02-13 10:55:41 +0000

    custom status line +

--- .config/nvim/lua/bledley/set.lua
+++ .config/nvim/lua/bledley/set.lua
@@ -1,43 +1,50 @@
+vim.cmd('autocmd VimEnter * silent! :intro')
 -- Transparent linenumbers column
 vim.cmd('hi! LineNr guibg=none ctermbg=none')
 -- Disable netrw at the very start of your init.lua
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
---vim.g.netrw_banner = 0
---vim.g.netrw_winsize = 15
---vim.g.netrw_browse_split = 0
---vim.g.netrw_keepdir = 0
---vim.g.netrw_localcopydircmd = 'cp -r'
 -- Nvim tree
 vim.g.nvim_tree_indent_markers = 1
 vim.g.netrw_browsex_viewer = 'xdg-open'
-vim.o.laststatus = 0
+vim.o.laststatus = 2
+-- linenumbers
 vim.o.nu = true
 vim.o.relativenumber = false
 vim.o.cursorline = true
+--vim.o.signcolumn = "yes"
 --vim.o.guicursor = ""
 --vim.o.showtabline =2
-vim.o.showmode = true
+--See Insert etc.
+vim.o.showmode = false
+--tabs and indent
 vim.o.tabstop = 4
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
 vim.o.autoindent = true
 vim.o.smartindent = true
+vim.o.scrolloff = 8
+-- line wrapping
 vim.o.wrap = true
---if i switch below to vim.o source errors?
+--tilde fringe off
 vim.opt.fillchars = { eob = ' ' }
 vim.o.swapfile = false
 vim.o.backup = false
 vim.o.undodir = os.getenv("HOME") .. "/.vim/undodir"
+--search
 vim.o.hlsearch = false
 vim.o.incsearch = true
 vim.o.ignorecase = true
 vim.o.smartcase = true
 vim.o.termguicolors = true
-vim.o.scrolloff = 8
---vim.o.signcolumn = "yes"
 vim.o.updatetime = 100
 vim.o.clipboard = "unnamedplus"
 vim.o.mouse = 'a'
 vim.o.breakindent = true
+vim.opt.conceallevel = 1
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.ruler = false
+-- Set the statusline to display the current mode in caps
+vim.o.statusline = '%#StatusLine# %{luaeval("vim.fn.mode():upper()")} %t %w %r %m %=%-14.(%l,%c%V%) %P'
