b172fb163f26f344cc86a11b9beb9f83f0491b1f
bleds1 <bleds1@users.noreply.github.com>
2024-02-13 10:54:19 +0000

    added new keymaps & cleanup

--- .config/nvim/lua/bledley/keymaps.lua
+++ .config/nvim/lua/bledley/keymaps.lua
@@ -1,10 +1,7 @@
 vim.g.mapleader = " "
--- File Explorer
-vim.keymap.set("n", "<leader>o-", vim.cmd.Ex)
+-- File Explorer (nvimtree)
 vim.keymap.set("n", "<leader>o-", ":e.<CR>")
---vim.keymap.set("n", "<leader>ol", vim.cmd.Hex)
-vim.keymap.set("n", "<leader>oo", vim.cmd.Lex)
-vim.keymap.set("n", "<leader>oo", vim.cmd.NvimTreeToggle)
+vim.keymap.set("n", "<leader>e", vim.cmd.NvimTreeToggle)
 vim.keymap.set("n", "<leader>,", vim.cmd.Files)
 vim.keymap.set("n", "<leader>.", vim.cmd.Buffers)
 vim.keymap.set("n", "<leader>/", vim.cmd.BLines)
@@ -15,19 +12,17 @@ vim.keymap.set("n", "<leader>bp", vim.cmd.bp)
 vim.keymap.set("n", "<leader>bk", vim.cmd.bd)
 vim.keymap.set("n", "<leader>en", vim.cmd.enew)
 --Splits
-vim.keymap.set("n", "<C-t>", vim.cmd.tabnew)
-vim.keymap.set("n", "<C-w>", vim.cmd.tabclose)
-vim.keymap.set("n", "<leader>wh", vim.cmd.split)
+vim.keymap.set("n", "<leader>ws", vim.cmd.split)
 vim.keymap.set("n", "<leader>wv", vim.cmd.vsplit)
---Navigate Splits
+--Navigate Window Splits
 vim.keymap.set("n", "<C-h>", "<C-w>h")
 vim.keymap.set("n", "<C-j>", "<C-w>j")
 vim.keymap.set("n", "<C-k>", "<C-w>k")
 vim.keymap.set("n", "<C-l>", "<C-w>l")
---Keep cursor in place for Ctrl d
+--Keep cursor in place for Ctrl d?
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
--- Write, Quit, Source
+-- Write, Quit, Close, Source
 vim.keymap.set("n", "<leader>w", ":w!<CR>")
 vim.keymap.set("n", "<leader>wq", ":wq!<CR>")
 vim.keymap.set("n", "<leader>fs", ":w!<CR>")
@@ -38,9 +33,8 @@ vim.keymap.set("n", "<leader>wc", vim.cmd.close)
 vim.keymap.set("n", "<leader>wx", vim.cmd.close)
 vim.keymap.set("n", "<leader>q", ":q<CR>")
 -- Tabs
-vim.keymap.set("n", "<C-t>", vim.cmd.tabnew)
-vim.keymap.set("n", "<C-w>", vim.cmd.tabclose)
--- Quick write
+--vim.keymap.set("n", "<C-t>", vim.cmd.tabnew)
+--vim.keymap.set("n", "<C-w>", vim.cmd.tabclose)
 -- Move lines in visual mode
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
@@ -64,3 +58,28 @@ vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv") -- move line down(v)
 -- Map Shift+Tab to outdent
 vim.api.nvim_set_keymap('i', '<S-Tab>', '<C-d>', { noremap = true })
 vim.api.nvim_set_keymap('v', '<S-Tab>', '<gv', { noremap = true })
+-- Set linebreak
+vim.keymap.set("n", "<leader>lb", ":set linebreak<CR>")
+vim.keymap.set("n", "<leader>nlb", ":set nolinebreak<CR>")
+--toggle linenumbers 
+vim.keymap.set("n", "<leader>tl", ":set nu!<CR>")
+-- Define a Lua function to get the word count
+function Get_word_count()
+  local word_count = vim.fn.wordcount().words
+  print("Word count: " .. word_count)
+end
+-- Set a keybinding to call the get_word_count function
+vim.api.nvim_set_keymap('n', '<leader>cw', ':lua Get_word_count()<CR>', { noremap = true, silent = true })
+-- Obsidian.nvim
+vim.keymap.set("n", "<leader>j", ":ObsidianToday<CR>")
+vim.keymap.set("n", "<leader>y", ":ObsidianYesterday<CR>")
+vim.keymap.set("n", "<leader>nn", ":ObsidianNew<CR>")
+vim.keymap.set("n", "<leader>r", ":ObsidianRename<CR>")
+vim.keymap.set("n", "<leader>is", ":ObsidianTemplate<CR>")
+vim.keymap.set("n", "<leader>os", ":ObsidianSearch<CR>")
+-- Markdown Preview
+vim.keymap.set("n", "<leader>mp", ":MarkdownPreview<CR>")
+-- Task popup
+vim.keymap.set("n", "<leader>nt", ":! task project:Next<CR>")
+vim.keymap.set("n", "<leader>t", ":! task<CR>")
+vim.keymap.set("n", "<leader>at", ":! task add ")
