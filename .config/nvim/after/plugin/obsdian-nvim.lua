b5231ff6f0351538df2c6f955d7702927746ec58
bleds1 <bleds1@users.noreply.github.com>
2024-02-14 21:03:39 +0000

    fixed ui issues in obsidian.nvim

--- .config/nvim/after/plugin/obsdian-nvim.lua
+++ .config/nvim/after/plugin/obsdian-nvim.lua
@@ -45,7 +45,6 @@ require("obsidian").setup({
     time_format = "%H:%M",
     -- A map for custom variables, the key should be the variable and the value a function
     substitutions = {},
-    
      },
     mappings = {
     ["gf"] = {
@@ -63,7 +62,37 @@ require("obsidian").setup({
   }
   },
     ui = {
-    enable = false,  -- set to false to disable all additional syntax features
+    enable = true,  -- set to false to disable all additional syntax features
+    update_debounce = 200,  -- update delay after a text change (in milliseconds)
+    -- Define how various check-boxes are displayed
+    checkboxes = {
+      -- NOTE: the 'char' value has to be a single character, and the highlight groups are defined below.
+      [" "] = { char = "☐", hl_group = "ObsidianTodo" },
+      ["x"] = { char = "✔", hl_group = "ObsidianDone" },
+      -- Replace the above with this if you don't have a patched font:
+      -- [" "] = { char = "☐", hl_group = "ObsidianTodo" },
+      -- ["x"] = { char = "✔", hl_group = "ObsidianDone" },
+
+      -- You can also add more custom ones...
+    },
+    -- Use bullet marks for non-checkbox lists.
+    bullets = { char = "•", hl_group = "ObsidianBullet" },
+    external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
+    -- Replace the above with this if you don't have a patched font:
+    -- external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
+    reference_text = { hl_group = "ObsidianRefText" },
+    highlight_text = { hl_group = "ObsidianHighlightText" },
+    tags = { hl_group = "ObsidianTag" },
+    hl_groups = {
+      -- The options are passed directly to `vim.api.nvim_set_hl()`. See `:help nvim_set_hl`.
+      ObsidianTodo = { bold = true, fg = "#6daad5" },
+      ObsidianDone = { bold = true, fg = "#89ddff" },
+      ObsidianBullet = { bold = true, fg = "#6daad5" },
+      ObsidianRefText = { underline = true, fg = "#c792ea" },
+      ObsidianExtLinkIcon = { fg = "#c792ea" },
+      ObsidianTag = { italic = true, fg = "#6daad5" },
+      ObsidianHighlightText = { bg = "#75662e" },
+    },
     },
   })
 
