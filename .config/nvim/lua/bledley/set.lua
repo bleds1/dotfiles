1a632f5262ca3d29058fa17f4c2b61b248129860
bleds1 <bleds1@users.noreply.github.com>
2024-02-15 22:29:15 +0000

    gui font? and moved statusline stuff to its own file

--- .config/nvim/lua/bledley/set.lua
+++ .config/nvim/lua/bledley/set.lua
@@ -4,6 +4,10 @@ vim.cmd('hi! LineNr guibg=none ctermbg=none')
 -- Disable netrw at the very start of your init.lua
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
+-- GUI font --NOTE:Is this actually working?
+if vim.fn.has('gui_running') == 1 then
+    vim.o.guifont = "JetBrains Mono:h13"
+end
 -- Nvim tree
 vim.g.nvim_tree_indent_markers = 1
 -- is this working?
@@ -47,45 +51,3 @@ vim.opt.conceallevel = 1
 vim.opt.splitbelow = true
 vim.opt.splitright = true
 vim.opt.ruler = false
--- Function to get git branch and icon in the statusline
--- TODO:Show changes
-function GetGitStatus()
-  local git_dir = vim.fn.finddir('.git', vim.fn.expand('%:p:h') .. ';')
-  if git_dir ~= '' then
-    local branch = vim.fn.systemlist('git -C ' .. vim.fn.fnameescape(vim.fn.expand('%:p:h')) .. ' rev-parse --abbrev-ref HEAD')[1]
-    local icon = ''  -- Unicode character for branch icon
-    return string.format('%s %s', icon, branch)
-  else
-    return ''
-  end
-end
--- Function to get full name for the statusline
-function GetFullModeName()
-  local mode = vim.fn.mode()
-  if mode == 'n' then
-    return 'NORMAL'
-  elseif mode == 'i' then
-    return 'INSERT'
-  elseif mode == 'v' then
-    return 'VISUAL'
-  elseif mode == 'V' then
-    return 'V-LINE'
-  elseif mode == '^V' then
-    return 'V-BLOCK'
-  elseif mode == 'R' then
-    return 'REPLACE'
-  elseif mode == 'c' then
-    return 'COMMAND'
-  else
-    return mode
-  end
-end
--- best working below
-vim.o.statusline = '%#StatusLine# %{luaeval("GetFullModeName()")}  %{luaeval("GetGitStatus()")} %t %w %r %m%=%-14.(Ln %l, Col %c%) '..vim.bo.filetype:sub(1,1):upper()..vim.bo.filetype:sub(2)..' '
--- Statusline with autocommand that updates on file change
- vim.cmd([[
-   augroup StatusLineUpdate
-     autocmd!
-     autocmd BufEnter * lua vim.wo.statusline = '%#StatusLine# %{luaeval("GetFullModeName()")}  %{luaeval("GetGitStatus()")} %t %w %r %m%=%-14.(Ln %l, Col %c%) '..vim.bo.filetype:sub(1,1):upper()..vim.bo.filetype:sub(2)..' '
-  augroup END
-]])
