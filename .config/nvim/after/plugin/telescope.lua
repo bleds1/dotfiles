ca5950de1903ad1de1469f0347460ac0f29f7e1f
bleds1 <bleds1@users.noreply.github.com>
2024-02-19 21:31:54 +0000

    leader,leader for buffers

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,3 +1,12 @@
+require('telescope').setup({
+    pickers  = {
+        current_buffer_fuzzy_find = {
+            previewer = false,
+        },
+    },
+})
+-- FIX: Disable preview in telescope
+
 local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
@@ -7,7 +16,8 @@ vim.keymap.set('n', '<leader>sr', builtin.oldfiles, {})
 vim.keymap.set('n', '<leader>sp', builtin.git_files, {})
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>bi', builtin.buffers, {})
-vim.keymap.set('n', '<leader>sB', builtin.buffers, {})
+vim.keymap.set('n', '<leader><leader>', builtin.buffers, {})
+vim.keymap.set('n', '<leader>sB', builtin.buffers, {}) -- probably not going to use this one
 vim.keymap.set('n', '<leader>sG', function()
 				builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
