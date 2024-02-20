158db9cc670e890496f629e620c07f162a9180a3
bleds1 <bleds1@users.noreply.github.com>
2024-02-20 18:36:39 +0000

    tweaked telescope keymaps

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,24 +1,33 @@
 require('telescope').setup({
     pickers  = {
-        current_buffer_fuzzy_find = {
+        oldfiles = {
+            previewer = false,
+            hidden = true,
+        },
+        find_files = {
+            previewer = false,
+            hidden = true,
+        },
+        live_grep = {
+            previewer = true,
+            hidden = true,
+        },
+        buffers = {
             previewer = false,
         },
     },
 })
--- FIX: Disable preview in telescope
 
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<leader>ff', ":Telescope find_files find_command=rg,--ignore,--hidden,--files,--glob,!.git<CR>") --testing alternate find command
--- vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>sf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fr', builtin.oldfiles, {})
 vim.keymap.set('n', '<leader>sr', builtin.oldfiles, {})
 vim.keymap.set('n', '<leader>sp', builtin.git_files, {})
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>bi', builtin.buffers, {})
-vim.keymap.set('n', '<leader><leader>', builtin.buffers, {})
-vim.keymap.set('n', '<leader>sB', builtin.buffers, {}) -- probably not going to use this one
-vim.keymap.set('n', '<leader>sG', function()
+vim.keymap.set('n', '<leader><leader>', builtin.oldfiles, {})
+vim.keymap.set('n', '<leader>fg', function() --  FIX: swapped this with git status - update whichkey
 				builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
