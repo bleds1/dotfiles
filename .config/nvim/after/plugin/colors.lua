c3376e08bfc922cca778c921b9e280e790f9d0d8
bleds1 <bleds1@users.noreply.github.com>
2024-02-29 00:28:26 +0000

    changed color of insert (modemsg) under statusline. Set show mode.

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -7,6 +7,8 @@ function ColorMyPencils(color)
             vim.api.nvim_set_hl(0.,"CursorLine", { bg = "#2B313C"})
 			-- vim.api.nvim_set_hl(0, "SignColumn", { bg = "#151515"}) -- needed for certain themes
             vim.api.nvim_set_hl(0, "Visual",{ bg = "#C290A0", fg = "#232323"})
+            -- Change color of mode message
+            vim.api.nvim_set_hl(0,"ModeMsg",{ fg = "#9da7b4" })
 end
 
 ColorMyPencils()
