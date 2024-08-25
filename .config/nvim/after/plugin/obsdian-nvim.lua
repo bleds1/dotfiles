a719e70f8ca758b9b0ed30a1248c5c9e71ed0eb2
bleds1 <bleds1@users.noreply.github.com>
2024-08-25 10:32:16 +0100

    202408251031 changed obsidian default dir

--- .config/nvim/after/plugin/obsdian-nvim.lua
+++ .config/nvim/after/plugin/obsdian-nvim.lua
@@ -20,7 +20,7 @@ require("obsidian").setup({
     -- Where to put new notes created from completion. Valid options are
     --  * "current_dir" - put new notes in same directory as the current buffer.
     --  * "notes_subdir" - put new notes in the default notes subdirectory.
-    new_notes_location = "current_dir",
+    new_notes_location = "~/vault/Fleeting/",
     -- Either 'wiki' or 'markdown'.
     preferred_link_style = "wiki",
     -- Control how wiki links are completed with these (mutually exclusive) options:
