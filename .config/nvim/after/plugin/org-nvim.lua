bc8c2e7c611bb0abdd3518f6fd24e05f730a0b98
bleds1 <bleds1@users.noreply.github.com>
2025-03-31 14:38:48 +0100

    202503311438 added nvim-org keyword faces

---
+++ .config/nvim/after/plugin/org-nvim.lua
@@ -0,0 +1,14 @@
+require('orgmode').setup({
+  org_todo_keywords = {'TODO', 'DOING', 'NEXT', 'WAITING', 'GOAL', 'PROJECT', '|', 'DONE', 'CANCELLED'},
+  org_todo_keyword_faces = {
+    TODO = ':foreground #cc4d3e',
+    DOING = ':foreground #1c7870',
+    NEXT = ':foreground #ff7f50',
+    WAITING = ':foreground #83898d',
+    GOAL = ':foreground #cc4d3e',
+    PROJECT = ':foreground #845bc8',
+    DONE = ':foreground #2b8c63',
+    CANCELLED = ':foreground #5d6265',
+  }
+
+})
