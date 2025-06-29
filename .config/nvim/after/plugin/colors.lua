abd8907994e0ec616e76623a35c4836772b88323
bleds1 <bleds1@users.noreply.github.com>
2025-06-29 11:27:13 +0100

    202506291127 nvim light theme

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -1,27 +1,27 @@
 function ColorMyPencils(color)
-			color = color or "github_dark_dimmed"
+			color = color or "github_light_default"
 			vim.cmd.colorscheme(color)
-			vim.api.nvim_set_hl(0, "Normal", { bg = "none", fg = "#acacac"}) -- needed for certain themes
-			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"}) -- needed for certain themes
-			vim.api.nvim_set_hl(0, "StatusLine", { bg = "none", fg = "#9da7b4"})
-            vim.api.nvim_set_hl(0.,"CursorLine", { bg = "#1c1c1c"})
-            vim.api.nvim_set_hl(0, 'StatusLineNC', { bg = 'none', fg = "#292929" })
+			-- vim.api.nvim_set_hl(0, "Normal", { bg = "none", fg = "#acacac"}) -- needed for certain themes
+			-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"}) -- needed for certain themes
+			-- vim.api.nvim_set_hl(0, "StatusLine", { bg = "none", fg = "#9da7b4"})
+   --          vim.api.nvim_set_hl(0.,"CursorLine", { bg = "#1c1c1c"})
+            -- vim.api.nvim_set_hl(0, 'StatusLineNC', { bg = 'none', fg = "#292929" })
 			-- vim.api.nvim_set_hl(0, "SignColumn", { bg = "#151515"}) -- needed for certain themes
-            vim.api.nvim_set_hl(0, "Visual",{ bg = "#C290A0", fg = "#1D1F2A"})
-            vim.api.nvim_set_hl(0,"ModeMsg",{ fg = "#9da7b4" })
-            vim.api.nvim_set_hl(0, "NormalNC", { bg = "#000000" })
-            vim.api.nvim_set_hl(0, "FloatBorder", { bg = "#000000" })
-            vim.api.nvim_set_hl(0, "TelescopePrompt", { bg = "#000000" })
-            vim.api.nvim_set_hl(0, "TelescopeSelection", { bg = "#000000", fg = "#9da7b4" })
-            vim.api.nvim_set_hl(0, "TelescopeResult", { bg = "#000000", fg = "#9da7b4" })
+            -- vim.api.nvim_set_hl(0, "Visual",{ bg = "#C290A0", fg = "#1D1F2A"})
+            -- vim.api.nvim_set_hl(0,"ModeMsg",{ fg = "#9da7b4" })
+            -- vim.api.nvim_set_hl(0, "NormalNC", { bg = "#000000" })
+            -- vim.api.nvim_set_hl(0, "FloatBorder", { bg = "#000000" })
+            -- vim.api.nvim_set_hl(0, "TelescopePrompt", { bg = "#000000" })
+            -- vim.api.nvim_set_hl(0, "TelescopeSelection", { bg = "#000000", fg = "#9da7b4" })
+            -- vim.api.nvim_set_hl(0, "TelescopeResult", { bg = "#000000", fg = "#9da7b4" })
             -- vim.cmd("hi! WhichKeyFloat guibg=#000000")
-            vim.cmd("hi! CmpDocumentation guibg=#000000")
-            vim.cmd("hi! CmpDocumentationBorder guibg=#000000")
-            vim.cmd("hi! CocExplorerNormalFloat guibg=#000000")
-            vim.cmd("hi! ColorColumn guibg=#000000")
-            vim.cmd("hi! PMenu guibg=#000000")
+            -- vim.cmd("hi! CmpDocumentation guibg=#000000")
+            -- vim.cmd("hi! CmpDocumentationBorder guibg=#000000")
+            -- vim.cmd("hi! CocExplorerNormalFloat guibg=#000000")
+            -- vim.cmd("hi! ColorColumn guibg=#000000")
+            -- vim.cmd("hi! PMenu guibg=#000000")
             --- italic comments and colour (terminal & font must be capable)
-            vim.api.nvim_set_hl(0, 'Comment', { italic=true, fg = "#555555" })
+            -- vim.api.nvim_set_hl(0, 'Comment', { italic=true, fg = "#555555" })
 end
 
 ColorMyPencils()
